eb41d78a61f811e9cf0630719771fbb624a47e3b
urfavteddybear <business.urfavteddybear@gmail.com>
2025-03-15 10:36:04 +0800

    feat: first commit

---
+++ src/oop/pertemuan/pkg2/OopPertemuan2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop.pertemuan.pkg2;
+
+/**
+ *
+ * @author User
+ */
+public class OopPertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Wahyu Putra";
+        int nim = 12345678;
+        
+        System.out.println("Nama saya:"+nama);
+        System.out.println("NIM : "+nim);
+        
+    }
+    
+}
