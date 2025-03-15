14017defde584d30252b66d3c2bc4cf286eda159
urfavteddybear <business.urfavteddybear@gmail.com>
2025-03-15 12:04:09 +0800

    feat: pertemuan 2

--- src/oop/pertemuan/pkg2/OopPertemuan2.java
+++ src/oop/pertemuan/pkg2/OopPertemuan2.java
@@ -4,6 +4,10 @@
  */
 package oop.pertemuan.pkg2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 /**
  *
  * @author User
@@ -16,10 +20,43 @@ public class OopPertemuan2 {
     public static void main(String[] args) {
         // TODO code application logic here
         String nama = "Wahyu Putra";
-        int nim = 12345678;
+        String nim = "12345678";
+        double ipk = 4.00;
+        String input;
         
-        System.out.println("Nama saya:"+nama);
+        BufferedReader NamaInput = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader NIMInput = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader IPKInput = new BufferedReader(new InputStreamReader(System.in));
+
+        
+        
+        System.out.print("Input nama anda: ");
+        try{
+            nama = NamaInput.readLine();
+        }catch(IOException e){
+            System.out.println("error");
+        }
+        
+        
+        System.out.print("Input NIM anda: ");
+        try{
+            nim = NIMInput.readLine();
+        }catch(IOException e){
+            System.out.println("error");
+        }
+        
+        System.out.print("Input IPK anda: ");
+        try{
+            ipk = Double.parseDouble(IPKInput.readLine());
+        }catch(IOException e){
+            System.out.println("error");
+        }
+        
+        
+        System.out.println("Nama saya: "+nama);
         System.out.println("NIM : "+nim);
+        
+        System.out.printf("Nama saya %s dengan NM %s dengan IPK %.2f", nama, nim, ipk);
         
     }
     
